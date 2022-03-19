7f5909b5b4ec159891b4ff5b079e04c146415794
Carlos Fonseca <carlosaacf@gmail.com>
2022-03-18 22:30:36 -0300

    puxando o readme

--- javaprofessional/src/fundamentos/PrimeiroPrograma.java
+++ javaprofessional/src/fundamentos/PrimeiroPrograma.java
@@ -3,7 +3,9 @@ package fundamentos;
 public class PrimeiroPrograma {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
 		
+		System.out.println("Hello World");
+			
 	}
 }
+
