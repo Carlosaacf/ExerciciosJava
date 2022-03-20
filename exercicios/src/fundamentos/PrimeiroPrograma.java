e0d9b0db75b3de4937ed024b3af4a240e97049d1
carlosaacf <carlosaacf@gmail.com>
2022-03-20 07:13:52 -0300

    Fundamentos Java

--- exercicios/src/fundamentos/PrimeiroPrograma.java
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -1,10 +1,33 @@
 package fundamentos;
 
+/**
+ * 
+ * @author Carlos Fonseca
+ * @since JDK1.0
+ * @see linkedin.com/in/carlosaacf
+ * 
+ *
+ */
+
 public class PrimeiroPrograma {
+
+	/**
+	 * 
+	 * @param args
+	 */
 
 	public static void main(String[] args) {
 
+		// uma sentença de codigo termina com ;
+		// Mais uma linha
+		// FIm
+
 		System.out.println("Primeiro Programa Parte #01!");
+
+		/*
+		 * comentarios de varias linhas devem ser feitos dessa forma
+		 * 
+		 */
 		System.out.println("Primeiro Programa Parte #02!");
 	}
 }
